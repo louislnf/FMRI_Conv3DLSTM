77047f4804c6dfc1aa7e58c0349e3bc6c2f41fba
Benoit <benoitcarlierg@gmail.com>
2019-03-27 16:08:55 +0100

    correction formats data browser

--- DatasetBrowser.py
+++ DatasetBrowser.py
@@ -2,6 +2,7 @@ import nibabel
 import os
 import re
 import numpy
+from random import randint
 
 class DatasetBrowser:
 
@@ -31,15 +32,20 @@ class DatasetBrowser:
         return sublist
     
     def getSubjectData(self, subject):
-        x_samples, y_samples = [], []
+        x_samples = numpy.zeros(shape=(5,15,64,64,24,1))
+        y_samples = numpy.zeros(shape=(5,4))
         pattern = re.compile(".*bold.nii.gz$")
         filesList = os.listdir("{0}/{1}/func/".format(self.dspath, subject))
+        a = 0
         for filename in filesList:
             if pattern.match(filename):
                 fmriFile = nibabel.load("{0}/{1}/func/{2}".format(self.dspath, subject, filename))
-                x_samples.append(numpy.array(fmriFile.dataobj).transpose((3,0,1,2)))
-
-                emotions = [[0,0,0,0]]*192
+                x_sample_to_add = numpy.array(fmriFile.dataobj).transpose((3,0,1,2))
+                x_sample_to_add = x_sample_to_add[:15]
+                x_sample_to_add = numpy.reshape(x_sample_to_add, (15,64,64,24,1))
+                x_samples[a] = x_sample_to_add
+                x_samples = numpy.array(x_samples)
+                emotions = [[0,0,0,0]]*15
                 emotionFilename = filename.replace("bold.nii.gz", "events.tsv")
                 f = open("{0}/{1}/func/{2}".format(self.dspath, subject, emotionFilename))
                 raw = f.read()
@@ -51,10 +57,12 @@ class DatasetBrowser:
                     onset, duration, emotion = float(data[0]), float(data[1]), data[2]
                     index = int(onset)//2
                     for i in range(index, int(onset+duration)//2+1):
-                        if i < 192:
+                        if i < 15:
                             emotions[i] = self.emotionMap[emotion]
-                y_samples.append(emotions)
-        return numpy.array(x_samples), numpy.array(y_samples)
+                i = randint(0,14)
+                y_samples[a] = emotions[i]
+        a+= 1
+        return (numpy.array(x_samples), numpy.array(y_samples))
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-#import keras
-import nibabel
-import numpy as np
-import re
-import os
 import keras
+import numpy as np
+
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 from BatchGenerator import *
-"""
+
+
+
 cnn = keras.Sequential()
 cnn.add(keras.layers.Conv3D(20, (4, 4, 4), input_shape=(64, 64, 24, 1)))
 cnn.add(keras.layers.MaxPooling3D(pool_size=(2,2,2)))
@@ -17,15 +19,17 @@ cnn.add(keras.layers.Flatten())
 cnn.compile(loss='mean_squared_error', optimizer='sgd')
 
 model = keras.Sequential()
-model.add(keras.layers.TimeDistributed(cnn, input_shape=(None, 192,64,64,24,1)))
-model.add(keras.layers.LSTM(64))
+model.add(keras.layers.TimeDistributed(cnn, input_shape=(15,64,64,24,1)))
+model.add(keras.layers.LSTM(8))
 model.add(keras.layers.Dense(4, activation="softmax"))
-"""
-generator = BatchGenerator("/Users/louislenief/Desktop/Projet IRM/ds000108-00002")
-"""
+
+generator = BatchGenerator("/home/benoit/Documents/Code/Projets/OpenFMRIDS")
+
+print(np.shape(generator[0][0][0]))
+
 model.compile(optimizer='rmsprop',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-#model.fit_generator(generator=generator)
-"""
+
+model.fit_generator(generator=generator)
